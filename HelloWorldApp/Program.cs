b9e48c038c6f90376bfe640b9edbb30d5e8c43e2
Barış Emre Yalçın <barisemre.bey@gmail.com>
2024-07-23 17:33:19 +0300

    complete HelloWorldApp

---
+++ HelloWorldApp/Program.cs
@@ -0,0 +1,29 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!"); // Satıra yazar ve diğer satıra devam eder
+
+//// Tek satır yorum
+//Console.ReadLine(); // Bir tuşa basmanı bekler
+
+///*
+// Çok satırlı yorum
+// */
+
+//Console.WriteLine("Hello, Barış!");
+//Console.Write("Hello, "); // Satırın sonunda imleç bekler, alt satıra geçmez
+//Console.ReadLine();
+
+// İki tam sayının toplamını hesaplayan ve ekrana yazdıran program
+//Console.WriteLine(24 + 8);
+
+// DegiskenVeriTipi DegiskenAdi = DegiskenDegeri
+//int sayi = 24;
+//Console.WriteLine("Sayı değerimiz: " + sayi);
+
+//int num1 = 24, num2 = 23;
+//int toplam = num1 + num2;
+//Console.WriteLine(toplam);
+
+int num1 = 24, num2 = 23;
+int toplam;
+toplam = num1 + num2;
+Console.WriteLine(toplam);
---
+++ IkiSayininKaresi/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number;
+int sqrNumber;
+
+Console.Write("Karesini almak istediğiniz sayıyı giriniz: ");
+string strNumber = Console.ReadLine();
+
+//number = Convert.ToInt32(strNumber);
+number = int.Parse(strNumber);
+
+sqrNumber = number * number;
+
+Console.WriteLine("Girdiğiniz sayının karesi: " + sqrNumber);
+Console.ReadLine(); // Uygulamayı durdurmak için
